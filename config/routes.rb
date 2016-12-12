49ad76f10c7968e8703a52439a810c3d957ddca7
Lien <messagelien@gmail.com>
2016-12-12 11:40:24 +1100

    dish page created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/restaurant'
-
+  get 'pages/dish'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
